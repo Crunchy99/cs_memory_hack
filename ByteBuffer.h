a86e722304c0b749e7ac8989fcfe9f9b9d1cf6df
Crunchy99 <crunchy9900@proton.me>
2025-04-09 21:53:43 +0100

    Update ByteBuffer.h

--- ByteBuffer.h
+++ ByteBuffer.h
@@ -3,38 +3,41 @@
 #include <msclr/gcroot.h>
 #define var auto
 typedef unsigned char byte;
+typedef unsigned long ulong;
 using namespace std;
 using namespace System;
 using namespace System::IO;
 using namespace System::Collections::Generic;
 
+#include "Ref.h"
 
 class ByteBuffer
 {
 public:
     byte* data;
-    ByteBuffer(long length);
+    ByteBuffer(ulong length);
 
-    string getString(long offset);
-    void setString(long offset, string value);
-    int getInt(long offset) const;
-    int& getInt(long offset);
-    void setInt(long offset, int value);
-    long getLong(long offset) const;
-    long& getLong(long offset);
-    void setLong(long offset, long value);
-    float getFloat(long offset) const;
-    float& getFloat(long offset);
-    void setFloat(long offset, float value);
-    double getDouble(long offset) const;
-    double& getDouble(long offset);
-    void setDouble(long offset, double value);
-    char getChar(long offset) const;
-    char& getChar(long offset);
-    void setChar(long offset, char value);
-    bool getBool(long offset) const;
-    byte& getBool(long offset);
-    void setBool(long offset, bool value);
+
+    string getString(ulong offset);
+    void setString(ulong offset, string value);
+    int getInt(ulong offset) const;
+    int& getInt(ulong offset);
+    void setInt(ulong offset, int value);
+    long getLong(ulong offset) const;
+    long& getLong(ulong offset);
+    void setLong(ulong offset, long value);
+    float getFloat(ulong offset) const;
+    float& getFloat(ulong offset);
+    void setFloat(ulong offset, float value);
+    double getDouble(ulong offset) const;
+    double& getDouble(ulong offset);
+    void setDouble(ulong offset, double value);
+    char getChar(ulong offset) const;
+    char& getChar(ulong offset);
+    void setChar(ulong offset, char value);
+    bool getBool(ulong offset) const;
+    byte& getBool(ulong offset);
+    void setBool(ulong offset, bool value);
 
 };
 
@@ -45,25 +48,25 @@ namespace Cpp
     {
     public:
         ::ByteBuffer* m_native;
-        ByteBuffer(long length);
+        ByteBuffer(ulong length);
 
         //static System::Func<String^, int>^ GetStrLen;
         //static System::Func<String^, int, char>^ IndexStr;
 
-        String^ getString(long offset);
-        void setString(long offset, String^ value);
-        int getInt(long offset);
-        void setInt(long offset, int value);
-        long getLong(long offset);
-        void setLong(long offset, long value);
-        float getFloat(long offset);
-        void setFloat(long offset, float value);
-        double getDouble(long offset);
-        void setDouble(long offset, double value);
-        char getChar(long offset);
-        void setChar(long offset, char value);
-        bool getBool(long offset);
-        void setBool(long offset, bool value);
+        String^ getString(ulong offset);
+        void setString(ulong offset, String^ value);
+        int getInt(ulong offset);
+        void setInt(ulong offset, int value);
+        long getLong(ulong offset);
+        void setLong(ulong offset, long value);
+        float getFloat(ulong offset);
+        void setFloat(ulong offset, float value);
+        double getDouble(ulong offset);
+        void setDouble(ulong offset, double value);
+        char getChar(ulong offset);
+        void setChar(ulong offset, char value);
+        bool getBool(ulong offset);
+        void setBool(ulong offset, bool value);
     };
 
 }
